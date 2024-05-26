20c2d8ca071728ff18f83ba2be1c425446379781
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-05-26 17:36:16 -0300

    feat: add ThemeToggle dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,25 @@
+import { ThemeProvider, ThemeToggle } from "@/components/_theme";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Outfit, IBM_Plex_Mono } from "next/font/google";
+import {
+  IBM_Plex_Mono as FontNono,
+  Outfit as FontSans,
+} from "next/font/google";
+import Image from "next/image";
 
-import "./globals.css";
+import "../styles/globals.css";
+import Link from "next/link";
 
-const outfit = Outfit({
+const fontSans = FontSans({
   weight: ["400", "500"],
   subsets: ["latin"],
+  variable: "--font-sans",
 });
 
-const ibmPlexMono = IBM_Plex_Mono({
+const fontNono = FontNono({
   weight: ["400", "500"],
   subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +34,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={outfit.className}>{children}</body>
+      <body className={cn(fontSans.variable, fontNono.variable)}>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <header>
+            <div className="container flex items-center justify-between pt-20">
+              <Link href="/">
+                <Image
+                  className="hidden dark:block"
+                  src="/logo-marcelo-pereira-frontend-developer-dark.svg"
+                  alt="Logo Marcelo Pereira"
+                  width={196}
+                  height={49}
+                  priority
+                />
+                <Image
+                  className="block dark:hidden"
+                  src="/logo-marcelo-pereira-frontend-developer-light.svg"
+                  alt="Logo Marcelo Pereira"
+                  width={196}
+                  height={49}
+                  priority
+                />
+              </Link>
+              <ThemeToggle />
+            </div>
+          </header>
+          <main className="flex min-h-screen flex-col gap-12 p-12">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-12 p-12">
-      <Image
-        src="/logo-marcelo-pereira-frontend-developer.svg"
-        alt="Logo Marcelo Pereira"
-        width={196}
-        height={49}
-        priority
-      />
-
-      <div className="">
-        <span className="text-xl lg:text-3xl text-[#9C9C9C]">
-          desenvolvimento & educação
-        </span>
-        <h1 className="text-3xl lg:text-8xl">
-          Tecnologia e expertise <br /> para seu projeto
-        </h1>
-      </div>
-    </main>
+    <div className="container">
+      <span className="block text-xl lg:text-3xl text-[#9C9C9C] font-mono font-medium mb-2">
+        desenvolvimento & educação
+      </span>
+      <h1 className="text-3xl lg:text-8xl">
+        Tecnologia e expertise <br /> para seu projeto
+      </h1>
+    </div>
   );
 }
