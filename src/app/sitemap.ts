b3582a6272f08c9772d15ef37e828e91a9624123
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-06-05 16:04:38 -0300

    fix: change sitemap file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.marcelopereira.dev',
+      url: 'https://www.marcelopereira.dev/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
