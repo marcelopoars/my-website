5c1bfb4be0d43ce247759b80430579b1b7839515
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-07-08 23:14:35 -0300

    refactor: edit Share component

--- src/components/share/share.tsx
+++ src/components/share/share.tsx
@@ -3,7 +3,7 @@
 import { IconBrandFacebook, IconBrandX, IconShare3 } from '@tabler/icons-react'
 import { Button } from '../ui'
 
-export function Share() {
+export async function Share() {
   const canonicalUrl = 'https://www.marcelopereira.dev/'
   const title = 'Marcelo Pereira | Desenvolvedor Frontend'
 
@@ -17,52 +17,52 @@ export function Share() {
   }
 
   const handleShare = async () => {
-    try {
-      await navigator.share(shareData)
-      console.log('Post shared successfully')
-    } catch (error) {
-      console.error('Error sharing the post:', error)
+    if (navigator.canShare(shareData)) {
+      try {
+        navigator.share(shareData)
+        console.log('Post shared successfully')
+      } catch (error) {
+        console.error('Error sharing the post:', error)
+      }
     }
   }
 
-  if (navigator.canShare && navigator.canShare(shareData)) {
-    return (
-      <aside className="flex items-center gap-2 mt-4">
+  return (
+    <>
+      <div className="flex items-center gap-2 mt-4 lg:hidden">
         Compartilhar
-        <Button variant={'outline'} size={'icon'} onClick={() => handleShare()}>
+        <Button variant={'outline'} size={'icon'} onClick={handleShare}>
           <IconShare3 />
         </Button>
-      </aside>
-    )
-  }
+      </div>
 
-  return (
-    <div className="flex items-center gap-1 lg:mt-4">
-      <span className="text-muted-foreground">Compartilhar no:</span>
-      <ul className="flex">
-        <li>
-          <a
-            className="inline-block p-1 hover:text-primary transition"
-            href={facebookURL}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Compartilhar no Facebook"
-          >
-            <IconBrandFacebook />
-          </a>
-        </li>
-        <li>
-          <a
-            className="inline-block p-1 hover:text-primary transition"
-            href={twitterUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Compartilhar no X"
-          >
-            <IconBrandX />
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div className="hidden lg:flex items-center gap-1 lg:mt-4">
+        <span className="text-muted-foreground">Compartilhar no:</span>
+        <ul className="flex">
+          <li>
+            <a
+              className="inline-block p-1 hover:text-primary transition"
+              href={facebookURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Compartilhar no Facebook"
+            >
+              <IconBrandFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              className="inline-block p-1 hover:text-primary transition"
+              href={twitterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Compartilhar no X"
+            >
+              <IconBrandX />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
