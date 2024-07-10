82b1e3571ceef02d6ecc32511bb9ff2da12f067b
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-07-09 22:09:37 -0300

    refactor: remove unused code

--- src/components/home/hero-home.tsx
+++ src/components/home/hero-home.tsx
@@ -1,4 +1,3 @@
-import { CurlyBracketsIcon, StackIcon } from '@/components'
 import { HeroLinks } from './_components'
 
 export function HeroHome() {
@@ -10,18 +9,7 @@ export function HeroHome() {
         </span>
 
         <h1 className="text-[32px] leading-tight sm:leading-tight sm:text-6xl lg:text-[100px] lg:leading-tight mb-10">
-          Tecnologia e expertise <br /> para seu{' '}
-          <span className="relative">
-            projeto
-            <div className="flex absolute -bottom-1 -right-[86px] sm:bottom-4 sm:-right-[130px] lg:-right-[260px]">
-              <div className="size-10 sm:size-14 lg:size-28 flex items-center justify-center bg-primary text-primary-foreground rounded-full">
-                <StackIcon className="size-3 sm:size-5 lg:size-auto" />
-              </div>
-              <div className="size-10 sm:size-14 lg:size-28 flex items-center justify-center bg-dica-do-nerd text-dica-do-nerd-foreground rounded-full -ml-2 lg:-ml-5">
-                <CurlyBracketsIcon className="size-3 sm:size-5 lg:size-auto" />
-              </div>
-            </div>
-          </span>
+          Tecnologia e expertise <br /> para seu projeto
         </h1>
 
         <HeroLinks />
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,3 @@
-export * from './curly-brackets-icon'
 export * from './github-icon'
 export * from './linkedin-icon'
-export * from './stack-icon'
 export * from './styled-components-icon'
