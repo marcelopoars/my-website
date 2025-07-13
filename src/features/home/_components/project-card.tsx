d601f3d30fe968f4efc419462b8414d1076ec19f
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2025-07-13 19:49:02 -0300

    feat(stack-info): create stackInfoMap for improved icon management

--- src/features/home/_components/project-card.tsx
+++ src/features/home/_components/project-card.tsx
@@ -1,18 +1,6 @@
-import { StyledComponentsIcon } from '@shared/components'
-import {
-  IconBrandCss3,
-  IconBrandGatsby,
-  IconBrandGithub,
-  IconBrandJavascript,
-  IconBrandNextjs,
-  IconBrandReact,
-  IconBrandTailwind,
-  IconBrandTypescript,
-  IconBrandVite,
-  IconWorldSearch,
-} from '@tabler/icons-react'
+import { stackInfoMap } from '@lib/stack-info'
+import { IconBrandGithub } from '@tabler/icons-react'
 import Image, { StaticImageData } from 'next/image'
-import { JSX } from 'react'
 
 interface ProjectCardProps {
   name: string
@@ -21,23 +9,6 @@ interface ProjectCardProps {
   githubLink: string
   previewLink: string
   stack: string[]
-}
-
-const stackInfoMap: Record<string, { icon: JSX.Element; label: string }> = {
-  css3: { icon: <IconBrandCss3 />, label: 'CSS3' },
-  gatsbyjs: { icon: <IconBrandGatsby />, label: 'Gatsby.js' },
-  githubapi: { icon: <IconBrandGithub />, label: 'GitHub' },
-  javascript: { icon: <IconBrandJavascript />, label: 'JavaScript' },
-  nextjs: { icon: <IconBrandNextjs />, label: 'Next.js' },
-  reactjs: { icon: <IconBrandReact />, label: 'React.js' },
-  seo: { icon: <IconWorldSearch />, label: 'SEO' },
-  styledcomponents: {
-    icon: <StyledComponentsIcon />,
-    label: 'Styled Components',
-  },
-  tailwindcss: { icon: <IconBrandTailwind />, label: 'Tailwind CSS' },
-  typescript: { icon: <IconBrandTypescript />, label: 'TypeScript' },
-  vitejs: { icon: <IconBrandVite />, label: 'Vite.js' },
 }
 
 export function ProjectCard({
