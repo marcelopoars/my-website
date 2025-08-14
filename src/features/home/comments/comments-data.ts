d0ea776b4ceb8cb6e7c70f40459bf13a4041a6ca
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2025-08-14 08:58:13 -0300

    feat: add new client testimonial to commentsData

--- src/features/home/comments/comments-data.ts
+++ src/features/home/comments/comments-data.ts
@@ -7,6 +7,12 @@ export type Comment = {
 export const commentsData: Comment[] = [
   {
     content:
+      'Trabalhar com Marcelo Pereira foi uma experiência excelente. Cumpriu todos os prazos combinados, manteve um valor justo e sempre entregou exatamente o que acordamos. Além disso, teve a paciência e atenção para me ouvir e entender minhas necessidades, conseguindo resolver problemas que outros profissionais não conseguiram. Recomendo de olhos fechados pelo profissionalismo e pela capacidade de transformar o que era difícil em soluções práticas e eficazes.',
+    client: 'Marcelo Solimeo',
+    role: 'Plataforma Jarvis Leilões',
+  },
+  {
+    content:
       'A dedicação e o profissionalismo do Marcelo são notáveis desde o primeiro contato. Ele não apenas entendeu nossas necessidades, mas também sugeriu soluções diferentes para a funcionalidade e manutenção do nosso site. Ficamos muito contentes com o resultado. Obrigada pelo excelente trabalho!',
     client: 'Luísa Trento',
     role: 'Diretora de marketing na CPD',
